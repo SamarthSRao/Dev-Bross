2342b375d43a5e1f31f907894482c0fc9212f4ce
Samarrrtthh <70063011+SamarthSRao@users.noreply.github.com>
2025-11-22 05:03:20 +0530

    Update success.vue

--- src/views/success.vue
+++ src/views/success.vue
@@ -1,15 +1,27 @@
-		</h5>
+<template>
+    <main class="cont">
+        <br />
+        <h1>Investment successful!</h1>
+        <h5 style="opacity: 0.8">
+            You have successfully invested
+            <span class="green">{{
+                $route?.query?.amt
+                    ?.toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            }}</span>
+            in <span class="green">{{ $route?.query?.company }}</span>.
+        </h5>
 
-		<h5 style="font-size: 1.2em">
-			<router-link :to="`/company/${$route?.query?.compuser}`">
-				View company &rarr;
-			</router-link>
-		</h5>
-		<br /><br /><br />
-		<br /><br /><br />
-		<br /><br /><br />
-		<br /><br />
-	</main>
+        <h5 style="font-size: 1.2em">
+            <router-link :to="`/company/${$route?.query?.compuser}`">
+                View company &rarr;
+            </router-link>
+        </h5>
+        <br /><br /><br />
+        <br /><br /><br />
+        <br /><br /><br />
+        <br /><br />
+    </main>
 </template>
 
 <script>
